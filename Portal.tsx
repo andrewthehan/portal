a8c83255b84df44613979f7447b58c90b132ee9e
Andrew Han <andrewthehan@gmail.com>
2022-02-24 20:38:06 -0800

    First commit.

---
+++ Portal.tsx
@@ -0,0 +1,52 @@
+import {
+  CSSProperties,
+  ForwardedRef,
+  forwardRef,
+  ReactNode,
+  useLayoutEffect,
+  useMemo
+} from "react";
+import { createPortal } from "react-dom";
+
+export type HTMLPortalElement = HTMLDivElement;
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+  style?: CSSProperties;
+  elementTag?: string;
+};
+
+export default forwardRef(function Portal(
+  { children, className, style, elementTag = "div" }: Props,
+  ref: ForwardedRef<HTMLPortalElement>
+) {
+  const container = useMemo(
+    () => document.createElement(elementTag),
+    [elementTag]
+  );
+
+  useLayoutEffect(() => {
+    const element = container;
+    document.body.appendChild(element);
+    return () => {
+      document.body.removeChild(element);
+    };
+  }, [container]);
+
+  const content = useMemo(
+    () => (
+      <div ref={ref} className={className} style={style}>
+        {children}
+      </div>
+    ),
+    [ref, className, style, children]
+  );
+
+  const portal = useMemo(
+    () => createPortal(content, container),
+    [content, container]
+  );
+
+  return portal;
+});
